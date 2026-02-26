946a1ef40c1c8adf4629c4b92eee55bda8ef0e1d
SirioRedGiant <Oiris1992@gmail.com>
2026-02-26 16:13:43 +0100

    bonus

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ console.log("Hello Oiris");
 
 const myName = process.argv[2] || "World";
 console.log(`Hello ${myName}`);
+
+const arrayDeiComandi = process.argv;
+const valoreInserito = arrayDeiComandi[2];
+
+if (!valoreInserito) {
+  console.log("Per favore, inserisci un argomento dopo il comando!");
+} else {
+  if (isNaN(valoreInserito)) {
+    console.log(`L'argomento '${valoreInserito}' non è un numero`);
+  } else {
+    console.log(
+      `Attenzione: '${valoreInserito}' è un numero. Mi serve una parola!`,
+    );
+  }
+}
